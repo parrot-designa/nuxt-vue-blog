c7714702fc26f65204f92968be5e20b5f7fb9c0c
wjb <wjb@wjbdeMac-mini.local>
2023-03-14 13:39:43 +0800

    fix:修改左边侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  title: "Hello VuePress",
+  title: "宝哥博客",
   description: "Just playing around",
   themeConfig: {
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Guide", link: "/guide/" },
-      { text: "External", link: "https://google.com" },
+    // nav: [
+    //   { text: "Home", link: "/" },
+    //   { text: "Guide", link: "/guide/" },
+    //   { text: "External", link: "https://google.com" },
+    //   {
+    //     text: "Languages",
+    //     items: [
+    //       { text: "Chinese", link: "/language/chinese" },
+    //       { text: "Japanese", link: "/language/japanese" },
+    //     ],
+    //   },
+    // ],
+    sidebar: [
+      {
+        title: "面试题",
+        collapsable: true,
+        children: [
+          {
+            title: "面试题",
+            collapsable: true,
+            children: ["/"],
+          },
+        ],
+      },
     ],
   },
 };
